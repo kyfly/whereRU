d220127756f1e3bdf1422f9c8a682be014442270
xuelei <2605121231@qq.com>
2016-01-28 23:59:52 +0800

    校园活动  二级嵌套

--- client/application/scripts/activity/activity-controller.js
+++ client/application/scripts/activity/activity-controller.js
@@ -1,7 +1,10 @@
 
 app.controller('ActivitiesController', ['$scope', function($scope){
-	
+
 }])
 app.controller('ActivitieController', ['$scope', function($scope){
-	
+
+}])
+app.controller('ActivitysController', ['$scope', function($scope){
+
 }])
