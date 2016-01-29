63d542e36b48336a233abfa984b39e7ddf7bde75
unknown <>
2016-01-29 14:52:07 +0800

    查询抢票余量完成

--- common/models/seckill.js
+++ common/models/seckill.js
@@ -10,6 +10,31 @@ module.exports = function(Seckill) {
 			path: '/:id/margin', verb: 'get'
 		}
 	});
-	Seckill.margin = function () {}
+	Seckill.margin = function (id, cb) {
+    Seckill.findById(id, function(err, seckill){
+      if(err) cb(err);
+      if (!seckill.toJSON()._seckillItems.length) {
+        seckill.seckillResults.count({get: true}, function (err, count) {
+          if(err) return cb(err);
+          var total = seckill.total;
+          var result = total - count;
+          cb(null,result);
+        });
+      } else {
+        var result = [];
+        seckill.seckillItems(function (err, items) {
+          if(err) return cb(err);
+          items.forEach(function (item) {
+            var item = item.toJSON();
+            result.push({
+              id:item.id,
+              margin:item.margin
+            });
+          });
+          cb(null,result);
+        });
+      }
+    });
+  };
 	Seckill.beforeRemote('prototype.__get__seckillResults', function(){});
 };
