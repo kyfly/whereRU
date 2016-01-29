600a9c6c45766ae516edbcaa901ab91e31bb6e87
lostwindtoku <849715057@qq.com>
2016-01-29 12:22:41 +0800

    圈子二级路由初步完成

--- client/application/scripts/coterie/coterie-controller.js
+++ client/application/scripts/coterie/coterie-controller.js
@@ -4,17 +4,20 @@ app.controller('CoteriesController', ['$scope', function($scope){
     {
       'id': 'message1',
       'display_name': '消息1',
-      'url': '/w/message1'
+      'url': '/w/message1',
+      'uiSref':'coteries.message1'
     },
     {
       'id': 'message2',
       'display_name': '消息2',
-      'url': '/w/message2'
+      'url': '/w/message2',
+      'uiSref':'coteries.message2'
     },
     {
       'id': 'message3',
       'display_name': '消息3',
-      'url': '/w/message3'
+      'url': '/w/message3',
+      'uiSref':'coteries.message3'
     }
   ];
 }]);
