af47ab74c872813e36af269984c924f9d26caae7
LiC <1078054388@qq.com>
2016-01-30 14:01:54 +0800

    user-router的更改

--- client/application/scripts/user/user-router.js
+++ client/application/scripts/user/user-router.js
@@ -1,28 +1,28 @@
-app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
-	$stateProvider
-	.state('login', {
-		url: '/w/login',
-		controller: 'LoginController',
-		templateUrl: 'application/views/user/login.html'
-	})
-	.state('confirmSchool', {
-		url: '/w/confirmSchool',
-		controller: 'ConfirmSchoolController',
-		templateUrl: 'application/views/confirm-school.html'
-	})
-	.state('register', {
-		url: '/w/register',
-		controller: 'RegisterController',
-		templateUrl: 'application/views/user/register.html'
-	})
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('login', {
+      url: '/w/login',
+      controller: 'LoginController',
+      templateUrl: 'application/views/user/login.html'
+    })
+    .state('confirmSchool', {
+      url: '/w/confirmSchool',
+      controller: 'ConfirmSchoolController',
+      templateUrl: 'application/views/confirm-school.html'
+    })
+    .state('register', {
+      url: '/w/register',
+      controller: 'RegisterController',
+      templateUrl: 'application/views/user/register.html'
+    })
     .state('index', {
-		url: '/',
-		controller: 'TestController',
-		templateUrl: '/application/scripts/views/index.html'
-	})
-  .state('management_system', {
-    url: '/w/team/:id',
-    controller: 'MSController',
-    template: '<div></div>'
-  })
+      url: '/',
+      controller: 'TestController',
+      templateUrl: '/application/views/index.html'
+    })
+    .state('management_system', {
+      url: '/w/team/:id',
+      controller: 'MSController',
+      template: '<div></div>'
+    })
 }])
