92b36cdbc29bced3a7c2faa2f445fc79d71c121d
xuelei <2605121231@qq.com>
2016-02-01 13:57:29 +0800

    校园团队（参数有问题）

--- client/application/scripts/team/team-controller.js
+++ client/application/scripts/team/team-controller.js
@@ -1,6 +1,16 @@
-app.controller('TeamsController', ['$scope', function($scope){
-	
-}])
-app.controller('TeamController', ['$scope', function($scope){
-	
-}])
+app.controller('TeamsController', ['$scope','Team', function ($scope, Team) {
+  console.log(new Date());
+  Team.getMySchoolTeams({
+      school: JSON.parse(localStorage.userInfo).user.school,
+      last: new Date('Mon Feb 01 2017 13:31:52 GMT+0800 (中国标准时间)')
+    }, function (res) {
+      console.log(res);
+      $scope.teamItems = res;
+    }, function () {
+    }
+  );
+
+}]);
+
+
+
