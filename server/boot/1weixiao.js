783a5a79b0efa5265e175a37dc542d0aa59ba0b1
Zhmoll <zhmoll@zhmoll.com>
2016-11-28 22:12:27 +0800

    初步支持微校配置接口

--- server/boot/1weixiao.js
+++ server/boot/1weixiao.js
@@ -4,29 +4,15 @@ const crypto = require('crypto');
 const _ = require('lodash');
 const getRawBody = require('raw-body');
 
-// function get_session_token($media_id) {
-//   session_start();
-//   $token = "";
-
-//   if ($_SESSION[$media_id] == $token) {
-//     $_SESSION[$media_id] = $this ->gen_token($media_id); //没有值赋新值
-//   }
-
-//   return $_SESSION[$media_id];
-// }
-
 function gen_token(media_id) {
   let secret = 'ETUANSTUDIO'; //自己定义SECRET值
   let string = media_id.length + media_id;
-
   const b = 64;
-
   secret = _.padRight(secret, b, '\0');
   const ipad = _.padRight('', b, '6');
   const opad = _.padRight('', b, '\\');
   const k_ipad = secret ^ ipad;
   const k_opad = secret ^ opad;
-
   return md5(k_opad + md5(k_ipad + string));
 }
 
@@ -40,10 +26,8 @@ function _cal_sign(param_array) {
   keys.forEach(function (key) {
     items.push(key + '=' + param_array[key]);
   });
-
   const api_secret = '71962700FE45352CF0C2A2833F8E6FA2'; //微校时提交给微校，32位字符串
   const str = items.join('&') + '&key=' + api_secret;
-
   return md5(str).toUpperCase();
 }
 
@@ -51,7 +35,6 @@ function open(app, req, res) {
   let body = req.body;
   const sign = body.sign;
   const timestamp = body.timestamp;
-
   delete (body['sign']);
   // 验证签名不通过
   if (sign != _cal_sign(body)) {
@@ -61,7 +44,6 @@ function open(app, req, res) {
     }
     return res.send(err);
   }
-
   const now = Date.parse(new Date()) / 1000;
   // 时间验证不通过
   const interval = now - timestamp;
@@ -72,16 +54,13 @@ function open(app, req, res) {
     }
     return res.send(err);
   }
-
   let result = {
     errcode: 0,
     errmsg: 'ok',
     token: '',
-    is_config: 0, // 暂时不提供配置页面
-    //custom: { school: 'SCHOOL_NAME' }
+    is_config: 1,
   }
   return res.send(result);
-  //   });
 }
 
 function close(app, req, res) {
@@ -138,7 +117,59 @@ function monitor(app, req, res) {
   res.send(req.query.echostr);
 }
 
+function config(app, req, res) {
+  const media_id = req.query.media_id;
+  const sign = req.query.sign;
+
+  let body = req.query;
+  delete (body['sign']);
+  delete (body['type']);
+  // 验证签名不通过
+  if (sign != _cal_sign(body)) {
+    let err = {
+      errcode: 5004,
+      errmsg: '签名错误'
+    }
+    return res.send(err);
+  }
+  const now = Date.parse(new Date()) / 1000;
+  // 时间验证不通过
+  const interval = now - timestamp;
+  if (Math.abs(interval) > 10) {
+    let err = {
+      errcode: 5003,
+      errmsg: '请求接口失败'
+    }
+    return res.send(err);
+  }
+  const token = gen_token(media_id);
+  app.models.weixiaoToken.upsert({ token: token, media_id: media_id }, (err, result) => {
+    if (err) return res.send({ errcode: 5003, errmsg: '请求接口失败' });
+    res.redirect('/weixiao/config?token=' + token);
+  });
+}
+
+function configSave(app, res, req) {
+  const body = req.body;
+  const media_id = body.media_id;
+  const teamId = body.teamId;
+  const token = body.token;
+  app.models.weixiaoToken.findOne({ token: token, media_id: media_id }, (err, tokenRecord) => {
+    if (err || !tokenRecord) return res.send({ errcode: 5003, errmsg: '令牌错误' });
+    app.models.Team.findById(teamId, (err, team) => {
+      if (err || !team) return res.send({ errcode: 5003, errmsg: '找不到该团队' });
+      team.media_id = media_id;
+      team.save();
+    });
+  });
+}
+
 module.exports = function (app) {
+  app.get('/weixiao/config', function (req, res, cb) {
+    const token = req.query.token;
+    res.send(res.sendFile(path.join(__dirname, '../../client/config.html')));
+  });
+
   app.post('/weixiao', function (req, res, cb) {
     getRawBody(req, (err, str) => {
       if (err) return cb(err);
@@ -148,8 +179,8 @@ module.exports = function (app) {
       switch (type) {
         case 'open': return open(app, req, res);
         case 'close': return close(app, req, res);
-        case 'config': break;// return config(app, req, res);
         case 'trigger': break;// return trigger();
+        case 'configSave': return configSave(app, req, res);
       }
     });
   });
@@ -157,6 +188,7 @@ module.exports = function (app) {
   app.get('/weixiao', function (req, res) {
     const type = req.query.type;
     switch (type) {
+      case 'config': return config(app, req, res);
       case 'monitor': return monitor(app, req, res);
     }
   });
