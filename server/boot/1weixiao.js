70aa4c8cebd625beef1ea47dc26845a2c5347acb
Zhmoll <zhmoll@zhmoll.com>
2016-11-28 20:30:05 +0800

    提供微校支持

---
+++ server/boot/1weixiao.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const crypto = require('crypto');
+const _ = require('lodash');
+const getRawBody = require('raw-body');
+
+// function get_session_token($media_id) {
+//   session_start();
+//   $token = "";
+
+//   if ($_SESSION[$media_id] == $token) {
+//     $_SESSION[$media_id] = $this ->gen_token($media_id); //没有值赋新值
+//   }
+
+//   return $_SESSION[$media_id];
+// }
+
+function gen_token(media_id) {
+  let secret = 'ETUANSTUDIO'; //自己定义SECRET值
+  let string = media_id.length + media_id;
+
+  const b = 64;
+
+  secret = _.padRight(secret, b, '\0');
+  const ipad = _.padRight('', b, '6');
+  const opad = _.padRight('', b, '\\');
+  const k_ipad = secret ^ ipad;
+  const k_opad = secret ^ opad;
+
+  return md5(k_opad + md5(k_ipad + string));
+}
+
+function md5(text) {
+  return crypto.createHash('md5').update(text).digest('hex');
+}
+
+function _cal_sign(param_array) {
+  let keys = Object.keys(param_array).sort();
+  let items = [];
+  keys.forEach(function (key) {
+    items.push(key + '=' + param_array[key]);
+  });
+
+  const api_secret = '71962700FE45352CF0C2A2833F8E6FA2'; //微校时提交给微校，32位字符串
+  const str = items.join('&') + '&key=' + api_secret;
+
+  return md5(str).toUpperCase();
+}
+
+function open(app, req, res) {
+  let body = req.body;
+  const sign = body.sign;
+  const timestamp = body.timestamp;
+
+  delete (body['sign']);
+  // 验证签名不通过
+  if (sign != _cal_sign(body)) {
+    let err = {
+      errcode: 5004,
+      errmsg: '签名错误'
+    }
+    return res.send(err);
+  }
+
+  const now = Date.parse(new Date()) / 1000;
+  // 时间验证不通过
+  const interval = now - timestamp;
+  if (Math.abs(interval) > 10) {
+    let err = {
+      errcode: 5003,
+      errmsg: '请求接口失败'
+    }
+    return res.send(err);
+  }
+
+  let result = {
+    errcode: 0,
+    errmsg: 'ok',
+    token: '',
+    is_config: 0, // 暂时不提供配置页面
+    //custom: { school: 'SCHOOL_NAME' }
+  }
+  return res.send(result);
+  //   });
+}
+
+function close(app, req, res) {
+  let body = req.body;
+  const sign = body.sign;
+  const timestamp = body.timestamp;
+
+  delete (body['sign']);
+  // 验证签名不通过
+  if (sign != _cal_sign(body)) {
+    let err = {
+      errcode: 5004,
+      errmsg: '签名错误'
+    }
+    return res.send(err);
+  }
+
+  const now = Date.parse(new Date()) / 1000;
+  // 时间验证不通过
+  const interval = now - timestamp;
+  if (Math.abs(interval) > 10) {
+    let err = {
+      errcode: 5003,
+      errmsg: '请求接口失败'
+    }
+    return res.send(err);
+  }
+
+  app.models.wechatmp.findOne({
+    where: {
+      media_id: body.media_id
+    }
+  }, function (err, ins) {
+    if (err) {
+      let error = {
+        errcode: 5003,
+        errmsg: '请求接口失败'
+      }
+      return res.send(error);
+    }
+
+    ins.isEnabled = false;
+    ins.save();
+
+    let result = {
+      errcode: 0,
+      errmsg: 'OK'
+    }
+    return res.send(result);
+  });
+}
+
+function monitor(app, req, res) {
+  res.send(req.query.echostr);
+}
+
+module.exports = function (app) {
+  app.post('/weixiao', function (req, res, cb) {
+    getRawBody(req, (err, str) => {
+      if (err) return cb(err);
+      req.body = JSON.parse(str.toString());
+      const type = req.query.type;
+      console.log(req.body);
+      switch (type) {
+        case 'open': return open(app, req, res);
+        case 'close': return close(app, req, res);
+        case 'config': break;// return config(app, req, res);
+        case 'trigger': break;// return trigger();
+      }
+    });
+  });
+
+  app.get('/weixiao', function (req, res) {
+    const type = req.query.type;
+    switch (type) {
+      case 'monitor': return monitor(app, req, res);
+    }
+  });
+};
--- server/boot/root.js
+++ server/boot/root.js
@@ -4,11 +4,11 @@ var bodyParser = require('body-parser');
 var crypto = require("crypto");
 var wechat = require('wechat-oauth');
 //var doc2html = require('../../modules/upload').docx2html;
-module.exports = function(server) {
-	server.use(bodyParser.urlencoded({
-	  extended: true
-	}));
-	server.use(bodyParser.json());
+module.exports = function (server) {
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({
+    extended: true
+  }));
   server.use('/ue/uploads', upload(server));
   server.use('/wechatUrl', function (req, res) {
     var client = new wechat(server.get('wechat').appID, server.get('wechat').appsecret);
@@ -18,7 +18,7 @@ module.exports = function(server) {
       created: new Date(),
       url: req.query.url,
       iswechat: isWechat
-    },function (err, ins) {
+    }, function (err, ins) {
       if (err)
         res.error(err);
       else {
@@ -30,15 +30,15 @@ module.exports = function(server) {
       }
     });
   })
-  server.get(/^\/w\/*/, function(req, res) {
+  server.get(/^\/w\/*/, function (req, res) {
     res.sendFile(path.join(__dirname, '../../client/index.html'));
   });
 
-  server.get(/^\/u\/*/, function(req, res) {
+  server.get(/^\/u\/*/, function (req, res) {
     res.sendFile(path.join(__dirname, '../../client/index.html'));
   });
 
-  server.get(/^\/MS\/*/, function(req, res) {
+  server.get(/^\/MS\/*/, function (req, res) {
     res.sendFile(path.join(__dirname, '../../client/management_system/index.html'));
   });
 };
